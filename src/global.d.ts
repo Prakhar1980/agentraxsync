8b29bad0bff796aeb766eb4daab6ca1aafc80a86
Prakhar1980 <150064371+Prakhar1980@users.noreply.github.com>
2026-04-19 17:47:34 +0530

    fix: typescript globals css import

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,8 +1,9 @@
 declare module "*.css";
+
 import type { Server as SocketIOServer } from "socket.io";
 
 declare global {
-  var io: SocketIOServer | null;
+  var io: SocketIOServer | undefined;
 }
 
 export {};
